15aa45e8b96ad49a30caaddd9a0cf6a30eb9f225
Baha Catherine Maigida <cathbaha1@gmail.com>
2024-12-04 15:25:37 +0100

    first-commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import logo from "../assets/raviKumarLogo.webp";
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+const NavBar =() => {
+  return (
+    <div className='flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+      <a href="/" aria-label='Home'>
+      {/* <img src={logo} className='mx-2' width={50} height={33} alt='Logo' /> */}
+      <p className='text-2xl font-semibold lg:text-4xl'>BCM</p>
+      </a>
+      </div>
+
+      <div className='mx-8 flex items-center justify-center gap-4 text-2xl'>
+            <a href="https://www.linkedin.com/in/maigida-catherine-a4a417188"
+                target='blank'
+                rel='noopener noreferer'      
+                aria-label='LinkedIn'      
+            >
+            <FaLinkedin />
+            </a>
+
+            <a href="https://github.com/catherine1997"
+                target='blank'
+                rel='noopener noreferer'      
+                aria-label='Github'      
+            >
+            <FaGithub />
+            </a>
+
+            <a href="https://www.instagram.com/in/cathybaha"
+                target='blank'
+                rel='noopener noreferer'      
+                aria-label='Instagram'      
+            >
+            <FaInstagram />
+            </a>
+
+            <a href="https://www.twitter.com/in/MaigidaCathy"
+                target='blank'
+                rel='noopener noreferer'      
+                aria-label='Twitter'      
+            >
+            <FaTwitter/>
+            </a>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/constants/index.js
@@ -0,0 +1,90 @@
+import project1 from "../assets/projects/Edupali.jpeg";
+import project2 from "../assets/projects/Bizflex app.jpeg";
+import project3 from "../assets/projects/portfolio.jpeg";
+import project4 from "../assets/projects/project-4.webp";
+
+export const HERO_CONTENT = `I am a Frontend Developer specialising in transforming user interface designs into functional systems using ReactJS and JavaScript. My primary responsibility is to bridge the gap between design concepts and practical web applications, ensuring seamless user experiences. I work collaboratively with design teams to implement pixel-perfect layouts and responsive interfaces that adhere to industry best practices. By leveraging my expertise in ReactJS and JavaScript, I bring designs to life, optimising performance and interactivity to create visually appealing and user-friendly web applications.`;
+
+export const ABOUT_TEXT = ` I am a Front End Developer with 4+ years of experience building and deploying scalable applications. Proficient in React Js, Javascript, Chakra UI, Tailwind Css, Bootstrap, HTML, CSS, Git(Version Control System) and Responsive Designs. I am a Women TechMakers Ambassador. contribute to breaking down barriers, challenging stereotypes, and fostering an environment where everyone, regardless of gender, can thrive in the field of technology`;
+
+export const EXPERIENCES = [
+  {
+    year: "05/2020 - Present ",
+    role: "Women Techmakers Ambassador",
+    company: "Women TechMakers",
+    description: `Led a team in organising and hosting Internation wowmens day celebration, Google Technology meetups. Coordinated volunteer program for organization, recruiting and training volunteers in best practices
+s and Represented organizations at professional conferences, delivering engaging presentations to diverse audiences`,
+    technologies: ["Public Speaking", "Good Communication Skills", "Content Skills", "Diversity and Inclusion"],
+  },
+  {
+    year: "10/2021 - Present",
+    role: "Frontend Developer",
+    company: "Aspilos Charity Foundation ",
+    description: `Co-ordinated and ensure that a project is on track, tasks are completed, and resources are effectively utilized, help facilitate and teach young women in web development (HTML, CSS and Javascript)
+Head a team in pitching a product and contributing to the development of the product(Bridges).Analyzed data related to projects in order to identify areas for improvement or optimization opportunities
+`,
+    technologies: ["HTML", "CSS", "Reactjs", "Javascript, Git/Github"],
+  },
+  {
+    year: " 07/2023 - 05/2024",
+    role: "Frontend Developer",
+    company: "Fastamoni Technologies Inc.",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Chakra UI. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["React Js", "Chakra UI", "Javascripts, Git/Github"],
+  },
+  {
+    year: "05/2021 - 10/2021",
+    role: "Volunteer(Front End Developer)",
+    company: "Aspilos Charity Foundation",
+    description: `Contributed to the development of web applications using HTML, CSS, Python and JavaScript. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Python", "HTML", "CSS", "Javascript"],
+  },
+
+  {
+    year: " 04/2018 - 10/2018 ",
+    role: "Front End Developer Intern",
+    company: " JD Lab ",
+    description: `Built a blogging system using Django Framework, where users can manage, publish and create blog post. Developed and maintained user-facing webpages using HTML, CSS, JavaScript and jQuery.
+`,
+    technologies: ["Python", "HTML", "CSS", "Javascript"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Edupali",
+    image: project1,
+    link: "https://www.edupali.com/",
+    description:
+      "EduPali by FastaMoni streamlines school fees payments for parents, while ensuring schools have funds for quality education. EduPali, FastaMoni, School fees payment solution, Affordability, Budget-friendly payments, Quality education, Parental convenience, Payment splitting, Streamlined payments, Secure payments, Multiple payment option, Automatic payment reminders, Online payments, Mobile payments, Transparent fees",
+    technologies: ["Chakra UI", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Bizflex",
+    image: project2,
+    link: "https://www.bizflex.africa/",
+    description:
+      "BizFlex is a financial management platform designed to streamline financial processes for businesses of all sizes. From registered companies and freelancers to even unregistered businesses, BizFlex offers a comprehensive suite to simplify money management, invoicing, savings, and business registration. BizFlex is the ideal financial management platform for businesses and freelancers seeking a comprehensive, user-friendly, and secure solution to manage their finances, invoicing, savings, and also business registration",
+    technologies: ["Chakra UI", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["TailwindCss", "CSS", "Javascript", "React"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+];
+
+export const CONTACT = {
+  address: "MTB/333 Moses Baha Residence, Anguwan Kadara Maitumbi, Minna, Niger State ",
+  phoneNo: "+234 816 074 9009 / +234 809 579 9350  ",
+  email: "cathbaha1@gmail.com",
+};
